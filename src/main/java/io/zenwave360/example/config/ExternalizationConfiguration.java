4299ae4d6cf5b65726c51be8598effbf0e616265
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2025-02-01 16:56:12 +0100

    updates `spring-modulith-events-scs` groupId and package for spring-modulith PR

--- src/main/java/io/zenwave360/example/config/ExternalizationConfiguration.java
+++ src/main/java/io/zenwave360/example/config/ExternalizationConfiguration.java
@@ -1,11 +1,11 @@
 package io.zenwave360.example.config;
 
-import io.zenwave360.modulith.events.scs.config.EnableSpringCloudStreamEventExternalization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.modulith.events.IncompleteEventPublications;
+import org.springframework.modulith.events.scs.config.EnableSpringCloudStreamEventExternalization;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
